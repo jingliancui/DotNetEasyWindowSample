7dde9485f1891360de09e4036263a19538442fa9
jingliancui <ericcui@qq.com>
2023-06-21 21:24:32 +0800

    Add codes

---
+++ DotNetEasyWindowSample /MAUI/SampleApp/SampleApp/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Maui.Embedding;
+using Microsoft.Maui.Platform;
+
+namespace SampleApp;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	private MauiContext mauiContext;
+
+    public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    protected override void OnAppearing()
+    {
+#if ANDROID
+        var builder = MauiApp.CreateBuilder();
+        builder.UseMauiEmbedding<Microsoft.Maui.Controls.Application>();
+        var mauiApp = builder.Build();
+        mauiContext = new MauiContext(mauiApp.Services, Microsoft.Maui.ApplicationModel.Platform.CurrentActivity);
+
+		var btn = new Button
+		{
+			Text="Hey!",
+			BackgroundColor=new Color(60,125,255)
+		};
+
+		var aOSBtn = btn.ToPlatform(mauiContext);
+
+		var win = new Com.Hjq.Window.EasyWindow(Microsoft.Maui.ApplicationModel.Platform.CurrentActivity);
+		win.SetContentView(aOSBtn);
+		win.SetDraggable();
+		
+		win.Show();
+#endif
+    }
+
+    private void OnCounterClicked(object sender, EventArgs e)
+	{
+		count++;
+
+		if (count == 1)
+			CounterBtn.Text = $"Clicked {count} time";
+		else
+			CounterBtn.Text = $"Clicked {count} times";
+
+		SemanticScreenReader.Announce(CounterBtn.Text);
+	}
+}
+
+
---
+++ DotNetEasyWindowSample /Xamarin/SampleApp/SampleApp/SampleApp.Android/MainActivity.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.OS;
+using Xamarin.Forms.Platform.Android;
+
+namespace SampleApp.Droid
+{
+    [Activity(Label = "SampleApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+
+            var btn = new Xamarin.Forms.Button
+            {
+                Text = "Hey!",
+                BackgroundColor = new Xamarin.Forms.Color(60, 125, 255)
+            };
+
+            var renderer = Platform.CreateRendererWithContext(btn, this);
+            var aOSView = renderer.View;
+
+            var win = new Com.Hjq.Window.EasyWindow(this);
+            win.SetContentView(aOSView);
+            win.SetDraggable();
+
+            win.Show();
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
